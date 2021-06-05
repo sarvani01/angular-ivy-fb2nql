978bdddc39a08c16564302ee1405cc9a302ad01c
sarvani01 <vidya.sarvani@gmail.com>
2021-06-05 19:53:52 +0530

    1 file added

---
+++ src/app/recipe.ts
@@ -0,0 +1,16 @@
+export class Recipe{
+  const  recipes =[ 
+{ 
+   name: 'Tasty Schnitzel',
+    description:'A super tasty Schnitzel - just awesome !!!',
+   imagePath: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG'
+},
+    { 
+   name: 'Big Fat burger',
+   description: 'what else you need to say???',
+   imagePath: 'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg'
+    }
+
+
+]; 
+}
