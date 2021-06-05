3f368d2b7661a54f4aed7c6466e407bd28d189dd
sarvani01 <vidya.sarvani@gmail.com>
2021-06-05 19:48:02 +0530

    1 file modified

--- src/app/mydir.directive.ts
+++ src/app/mydir.directive.ts
@@ -17,28 +17,28 @@ export class MydirDirective implements OnInit {
   @HostBinding('style.backgroundColor') bg: string = '';
   constructor(private render: Renderer2, private element: ElementRef) {}
   ngOnInit() {
-    //this.bg = 'green';
+    this.bg = 'green';
     this.render.setAttribute(this.element.nativeElement, 'align', 'center');
     this.render.setStyle(this.element.nativeElement, 'color', 'white');
   }
-  @HostListener('mouseenter') mouseover() {
-    // this.renders.setStyle(this.element.nativeElement,'background-color',this.Dcolor);
-    if (this.ec == '') {
-      this.ec = 'lightblue';
-    }
-    this.bg = this.ec;
-    this.render.setAttribute(this.element.nativeElement, 'align', 'center');
-    this.render.setStyle(this.element.nativeElement, 'font-family', 'verdana');
-    this.render.setStyle(this.element.nativeElement, 'color', 'whitesmoke');
-  }
-  @HostListener('mouseleave') mouseleave() {
-    //  this.renders.setStyle(this.element.nativeElement,'background-color',this.Acolor);
-    if (this.lc == '') {
-      this.lc = 'transparent';
-    }
-    this.bg = this.lc;
-    this.render.setAttribute(this.element.nativeElement, 'align', 'center');
-    this.render.setStyle(this.element.nativeElement, 'font-family', 'verdana');
-    this.render.setStyle(this.element.nativeElement, 'color', this.lc);
-  }
+  // @HostListener('mouseenter') mouseover() {
+  //   // this.renders.setStyle(this.element.nativeElement,'background-color',this.Dcolor);
+  //   if (this.ec == '') {
+  //     this.ec = 'lightblue';
+  //   }
+  //   this.bg = this.ec;
+  //   this.render.setAttribute(this.element.nativeElement, 'align', 'center');
+  //   this.render.setStyle(this.element.nativeElement, 'font-family', 'verdana');
+  //   this.render.setStyle(this.element.nativeElement, 'color', 'whitesmoke');
+  // }
+  // @HostListener('mouseleave') mouseleave() {
+  //   //  this.renders.setStyle(this.element.nativeElement,'background-color',this.Acolor);
+  //   if (this.lc == '') {
+  //     this.lc = 'transparent';
+  //   }
+  //   this.bg = this.lc;
+  //   this.render.setAttribute(this.element.nativeElement, 'align', 'center');
+  //   this.render.setStyle(this.element.nativeElement, 'font-family', 'verdana');
+  //   this.render.setStyle(this.element.nativeElement, 'color', this.lc);
+  // }
 }
