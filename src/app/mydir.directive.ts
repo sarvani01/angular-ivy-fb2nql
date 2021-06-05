99b77354924c6928e370828f1f446a7faab2b645
sarvani01 <vidya.sarvani@gmail.com>
2021-06-05 20:24:02 +0530

    1 file modified

--- src/app/mydir.directive.ts
+++ src/app/mydir.directive.ts
@@ -12,16 +12,21 @@ import {
   selector: '[appMydir]'
 })
 export class MydirDirective implements OnInit {
-  @Input() ec: string = '';
-  @Input() lc: string = '';
+  // @Input() ec: string = '';
+  // @Input() lc: string = '';
   @HostBinding('style.backgroundColor') bg: string = '';
   constructor(private render: Renderer2, private element: ElementRef) {}
   ngOnInit() {
     this.bg = 'green';
     this.render.setAttribute(this.element.nativeElement, 'align', 'center');
     this.render.setStyle(this.element.nativeElement, 'color', 'white');
+    this.render.setStyle(
+      this.element.nativeElement,
+      'backgroundColor',
+      'green'
+    );
   }
-  
+
   // @HostListener('mouseenter') mouseover() {
   //   // this.renders.setStyle(this.element.nativeElement,'background-color',this.Dcolor);
   //   if (this.ec == '') {
