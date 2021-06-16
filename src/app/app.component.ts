f16e2e1c32a3ed62331f39ea60047fcfed737b5c
sarvani01 <vidya.sarvani@gmail.com>
2021-06-16 22:14:04 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
   pText = 'Start editing to see some magic happen :)';
   txt: string = '';
   show: boolean = false;
-  evenumbers = [2, 4, 6, 8, 10];
+  numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   passtochild() {
     this.show = true;
 
